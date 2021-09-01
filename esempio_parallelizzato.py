a87881176187da2219b3a85991c53463deeec1ff
AndBetter <87365851+AndBetter@users.noreply.github.com>
2021-09-01 17:08:50 +0200

    Update esempio_parallelizzato.py

--- esempio_parallelizzato.py
+++ esempio_parallelizzato.py
@@ -3,29 +3,31 @@ from numpy import random
 import numpy as np
 
 def somma(minimo,massimo,N):
-    x= sum(random.randint(minimo,massimo,N))/N
-    y= sum(random.randint(minimo,massimo,N)**2)/N
-    return(x,y)
+    x_1= sum(random.randint(minimo,massimo,N))/N
+    x_2= sum(random.randint(minimo,massimo,N))/N
+    y_1= sum(random.randint(minimo,massimo,N)**2)/N
+    y_2= sum(random.randint(minimo,massimo,N)**2)/N
+    return(x_1,x_2,y_1,y_2)
 
 
 def main():
-    pool = multiprocessing.Pool(16)
-    results = pool.starmap(somma, [(-1, mxx, 10000) for mxx in range(16)])
+    pool = multiprocessing.Pool(8)
+    results = pool.starmap(somma, [(-1, mxx, 100000) for mxx in range(16)])
 
     pool.close()
     pool.join()
-
-    RESULTS=[]
-    for result in results:
-        # prints the result string in the main process
-        print(result)
-        RESULTS.append(result)
-        
-    return RESULTS
-
-        
-
+          
+    return results
+ 
+         
+ 
 if __name__ == '__main__':
-    # Better protect your main function when you use multiprocessing
-    risultati=main()
-    risultati=np.array(risultati)
+     # Better protect your main function when you use multiprocessing
+     risultati=np.stack(main(),axis=1)
+     
+     X=risultati[0:2,:]
+     Y=risultati[2:4,:]
+
+
+
+
